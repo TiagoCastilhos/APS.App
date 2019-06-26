38225f00c04b9298c588ba1466467c9e0f16f047
TiagoCastilhos <tiagocastilhos66@gmail.com>
2019-06-25 23:56:46 -0300

    started the party

---
+++ src/Components/ListRules.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Container, Header, Content, Card, CardItem, Body, Text } from 'native-base';
+
+class ListRules extends Component {
+    state = {
+        rules: []
+    }
+
+    return(
+
+    )
+}
--- src/Components/NewRule.js
+++ src/Components/NewRule.js
@@ -1,27 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, View, TextInput, Text } from 'react-native';
-import { Container, Label, Content, Item, Input, Button } from 'native-base';
+import { Container, List, ListItem, Content, Item, Input, Button } from 'native-base';
 
-const NewRule = () => {
-    return (
+export default class NewRule extends Component {
+    state = {
+        mostPopularRules: [
+            { key: 1, url: "https://google.com" },
+            { key: 2, url: "https://youtube.com" },
+            { key: 3, url: "https://www.netflix.com/" }
+        ],
+        urlToCreateRule: ''
+    } 
+
+    changeUrlHandler = (text) => {
+        this.setState({
+            urlToCreateRule: text
+        });
+    }
+
+    createRule = () => {
+        if (this.state.urlToCreateRule !== ''){
+            const body = {
+                'relationshipId': 1,
+                'siteUri': this.state.urlToCreateRule
+            }
+            fetch('https://accessblockerapi.azurewebsites.net/Rules', { method: 'POST', body: body })
+            .catch((r) => alert('Erro ao criar regra. Erro: ' + r.status))
+        }        
+    }
+
+    render(){
+        return (
         <Container>
             <Content>
                 <Text style={styles.label}>Digite a URL:</Text>
                 <Item regular>
-                    <Input style={styles.input} placeholder="Exemplo: https://youtube.com" />
+                    <Input style={styles.input}
+                        placeholder="Exemplo: https://youtube.com"
+                        value={ this.state.urlToCreateRule }
+                        onChangeText={(text) => this.changeUrlHandler(text)}/>
                 </Item>
-                <Button block style={styles.button}>
+                <Button block style={styles.button} onPress={() => this.createRule()}>
                     <Text style={styles.buttonText}>Criar regra</Text>
                 </Button>
+                <List>
+                    <Text style={styles.popularRules}>URLs mais populares</Text>
+                    {this.state.mostPopularRules.map(r => 
+                    <ListItem key={r.key} onPress onPress={() => this.changeUrlHandler(r.url)}>
+                        <Text>{r.url}</Text>
+                    </ListItem>)}
+                </List>
             </Content>
         </ Container>
-    )
+    )};
 }
 
 const styles = StyleSheet.create({
     label: {
         fontFamily: 'Roboto',
         fontSize: 38,
+        textAlign: 'center',
+        margin: 20
+    },
+    popularRules: {
+        fontFamily: 'Roboto',
+        fontSize: 20,
         textAlign: 'center',
         margin: 20
     },
@@ -39,5 +82,3 @@ const styles = StyleSheet.create({
         color: 'white'
     }
 })
-
-export default NewRule;
